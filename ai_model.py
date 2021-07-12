354e5056cb133f3ba7ff247952649437ef0948cc
Master-Fortune <85618964+Master-Fortune@users.noreply.github.com>
2021-07-12 15:05:09 +0200

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -17,7 +17,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy
-pip install sklearn
+#pip install sklearn
 from scikitlearn.metrics import classification_report
 
 #Christopher Katondo
