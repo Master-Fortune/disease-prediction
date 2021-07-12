1acc2ab084850e81e2d46df956e56a71da232b36
Master-Fortune <85618964+Master-Fortune@users.noreply.github.com>
2021-07-12 14:54:44 +0200

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -17,7 +17,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy
-#import sklearn
+from sklearn.metrics import classification_report
 
 #Christopher Katondo
 #Noralle Matanda
